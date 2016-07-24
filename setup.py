81015a55e42a3d713ed562c72ca5c1f4fe5190a5
Ahmet Taspinar <taspinar@gmail.com>
2016-07-24 13:16:49 +0200

    update version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 		name='twitterscraper',
-		version='0.1.3',
+		version='0.1.3.1',
 		description='Tool for scraping Tweets',
 		url='https://github.com/taspinar/twitterscraper',
 		author='Ahmet Taspinar',

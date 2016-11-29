38ea681d798c62f49eb2f3698dfe3869cc94b1f8
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-11-29 15:18:08 +0100

    twitterscraper: Use custom JSON encoder
    Closes https://github.com/taspinar/twitterscraper/issues/13

--- twitterscraper/main.py
+++ twitterscraper/main.py
@@ -1,13 +1,34 @@
 """
 This is a command line application that allows you to scrape twitter!
 """
-
+import collections
+import json
 from argparse import ArgumentParser
+from datetime import datetime
 from os.path import isfile
 from json import dump
 import logging
 
 from twitterscraper import query_tweets
+
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if hasattr(obj, '__json__'):
+            return obj.__json__()
+        elif isinstance(obj, collections.Iterable):
+            return list(obj)
+        elif isinstance(obj, datetime):
+            return obj.isoformat()
+        elif hasattr(obj, '__getitem__') and hasattr(obj, 'keys'):
+            return dict(obj)
+        elif hasattr(obj, '__dict__'):
+            return {member: getattr(obj, member)
+                    for member in dir(obj)
+                    if not member.startswith('_')}
+
+        return json.JSONEncoder.default(self, obj)
 
 
 def main():
@@ -31,6 +52,6 @@ def main():
 
         tweets = query_tweets(args.query, args.limit)
         with open(args.output, "w") as output:
-            dump(tweets, output)
+            dump(tweets, output, cls=JSONEncoder)
     except KeyboardInterrupt:
         logging.info("Program interrupted by user. Quitting...")

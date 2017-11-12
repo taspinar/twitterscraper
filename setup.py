a61dac85621e8418617ba9468aad2e1542450f91
Ahmet Taspinar <taspinar@gmail.com>
2017-11-12 13:28:11 +0100

    update version number in setup file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='twitterscraper',
-    version='0.3.1',
+    version='0.3.2',
     description='Tool for scraping Tweets',
     url='https://github.com/taspinar/twitterscraper',
     author=['Ahmet Taspinar', 'Lasse Schuirmann'],

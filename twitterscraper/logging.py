40a3a0d2883be16751ad20388eef1c05eae6c4d2
andrew <andrew>
2018-05-30 21:49:02 -0400

    use formatter

--- twitterscraper/logging.py
+++ twitterscraper/logging.py
@@ -2,5 +2,9 @@ import logging
 
 
 logger = logging.getLogger('twitterscraper')
-handler = logging.Handler(format='%(levelname)s: %(message)s', level=logging.INFO)
-logger.addHandler(handler)
+
+formatter = logging.Formatter('%(levelname)s: %(message)s')
+logger.setFormatter(formatter)
+
+level = logging.INFO
+logger.setLevel(level)

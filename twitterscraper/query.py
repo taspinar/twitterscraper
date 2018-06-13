a1055d92de03b4c4a3c9732fb9556880d2ef59a7
Ahmet Taspinar <taspinar@gmail.com>
2018-06-13 20:49:27 +0200

    query.py : remove fake_useragent
    useragents will no longer be generated with the fake_useragent package.
    At the moment seven of the most popular useragent strings are written hardcoded in query.py
    In later stages this should be done via a separate module.
    
    This fixes https://github.com/taspinar/twitterscraper/issues/118,
    https://github.com/taspinar/twitterscraper/issues/115,
    https://github.com/taspinar/twitterscraper/issues/90

--- twitterscraper/query.py
+++ twitterscraper/query.py
@@ -6,13 +6,17 @@ import json
 from functools import partial
 from multiprocessing.pool import Pool
 
-from fake_useragent import UserAgent
 from twitterscraper.tweet import Tweet
 from twitterscraper.logging import logger
 
-
-ua = UserAgent()
-HEADERS_LIST = [ua.chrome, ua.google, ua['google chrome'], ua.firefox, ua.ff]
+ua1 = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+ua2 = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+ua3 = "Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13"
+ua4 = "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko"
+ua5 = "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201"
+ua6 = "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16"
+ua7 = "Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre"
+HEADERS_LIST = [ua1, ua2, ua3, ua4, ua5, ua6, ua7]
 
 INIT_URL = "https://twitter.com/search?f=tweets&vertical=default&q={q}&l={lang}"
 RELOAD_URL = "https://twitter.com/i/search/timeline?f=tweets&vertical=" \

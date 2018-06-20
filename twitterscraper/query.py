fe6f75e9fdabc22ce8e8b566add257458f0ba5f4
Ahmet Taspinar <taspinar@gmail.com>
2018-06-20 22:23:40 +0200

    Remove fakeuseragent (#119)
    * query.py : remove fake_useragent, move separate try / except
    
    - useragents will no longer be generated with the fake_useragent package.
    At the moment seven of the most popular useragent strings are written hardcoded in query.py
    In later stages this should be done via a separate module.
    -By removing the separate try / except for the JsonDecodeError, each process now also continues with retrying to get the data instead of exiting.
    
    This fixes https://github.com/taspinar/twitterscraper/issues/118,
    https://github.com/taspinar/twitterscraper/issues/115,
    https://github.com/taspinar/twitterscraper/issues/90

--- twitterscraper/query.py
+++ twitterscraper/query.py
@@ -6,13 +6,18 @@ import json
 from functools import partial
 from multiprocessing.pool import Pool
 
-from fake_useragent import UserAgent
 from twitterscraper.tweet import Tweet
 from twitterscraper.logging import logger
 
+HEADERS_LIST = ["Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1",
+                "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+                "Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13",
+                "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
+                "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201",
+                "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
+                "Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre"]
 
-ua = UserAgent()
-HEADERS_LIST = [ua.chrome, ua.google, ua['google chrome'], ua.firefox, ua.ff]
+HEADER = {'User-Agent': random.choice(HEADERS_LIST)}
 
 INIT_URL = "https://twitter.com/search?f=tweets&vertical=default&q={q}&l={lang}"
 RELOAD_URL = "https://twitter.com/i/search/timeline?f=tweets&vertical=" \
@@ -37,10 +42,9 @@ def query_single_page(url, html_response=True, retry=10):
     :param retry: Number of retries if something goes wrong.
     :return: The list of tweets, the pos argument for getting the next page.
     """
-    headers = {'User-Agent': random.choice(HEADERS_LIST)}
 
     try:
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=HEADER)
         if html_response:
             html = response.text or ''
         else:
@@ -72,7 +76,7 @@ def query_single_page(url, html_response=True, retry=10):
     except json.decoder.JSONDecodeError as e:
         logger.exception('Failed to parse JSON "{}" while requesting "{}".'.format(
             e, url))
-
+        
     if retry > 0:
         logger.info("Retrying... (Attempts left: {})".format(retry))
         return query_single_page(url, html_response, retry-1)

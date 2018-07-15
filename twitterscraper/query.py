ddac47f010d66d9f93fefa5b770b878106300275
andrew <andrew>
2018-07-15 18:19:53 -0400

    be consistent in the way strings are single-quoted

--- twitterscraper/query.py
+++ twitterscraper/query.py
@@ -10,20 +10,20 @@ from twitterscraper.tweet import Tweet
 from twitterscraper.logging import logger
 
 HEADERS_LIST = [
-    "Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13",
-    "Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko",
-    "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201",
-    "Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16",
-    "Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre"
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13',
+    'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
+    'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
+    'Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre'
 ]
 
 HEADER = {'User-Agent': random.choice(HEADERS_LIST)}
 print('HEADER', HEADER)
 
-INIT_URL = "https://twitter.com/search?f=tweets&vertical=default&q={q}&l={lang}"
-RELOAD_URL = "https://twitter.com/i/search/timeline?f=tweets&vertical=" \
-             "default&include_available_features=1&include_entities=1&" \
-             "reset_error_state=false&src=typd&max_position={pos}&q={q}&l={lang}"
+INIT_URL = 'https://twitter.com/search?f=tweets&vertical=default&q={q}&l={lang}'
+RELOAD_URL = 'https://twitter.com/i/search/timeline?f=tweets&vertical=' \
+             'default&include_available_features=1&include_entities=1&' \
+             'reset_error_state=false&src=typd&max_position={pos}&q={q}&l={lang}'
 
 def linspace(start, stop, n):
     if n == 1:
@@ -64,7 +64,7 @@ def query_single_page(url, html_response=True, retry=10):
         if not html_response:
             return tweets, json_resp['min_position']
 
-        return tweets, "TWEET-{}-{}".format(tweets[-1].id, tweets[0].id)
+        return tweets, 'TWEET-{}-{}'.format(tweets[-1].id, tweets[0].id)
     except requests.exceptions.HTTPError as e:
         logger.exception('HTTPError {} while requesting "{}"'.format(
             e, url))
@@ -79,10 +79,10 @@ def query_single_page(url, html_response=True, retry=10):
             e, url))
 
     if retry > 0:
-        logger.info("Retrying... (Attempts left: {})".format(retry))
+        logger.info('Retrying... (Attempts left: {})'.format(retry))
         return query_single_page(url, html_response, retry-1)
 
-    logger.error("Giving up.")
+    logger.error('Giving up.')
     return [], None
 
 
@@ -103,8 +103,8 @@ def query_tweets_once_generator(query, limit=None, lang=''):
     :return:      A list of twitterscraper.Tweet objects. You will get at least
                   ``limit`` number of items.
     """
-    logger.info("Querying {}".format(query))
-    query = query.replace(' ', '%20').replace("#", "%23").replace(":", "%3A")
+    logger.info('Querying {}'.format(query))
+    query = query.replace(' ', '%20').replace('#', '%23').replace(':', '%3A')
     pos = None
     num_tweets = 0
     try:
@@ -115,7 +115,7 @@ def query_tweets_once_generator(query, limit=None, lang=''):
                 pos is None
             )
             if len(new_tweets) == 0:
-                logger.info("Got {} tweets for {}.".format(
+                logger.info('Got {} tweets for {}.'.format(
                     num_tweets, query))
                 return
 
@@ -125,17 +125,17 @@ def query_tweets_once_generator(query, limit=None, lang=''):
             num_tweets += len(new_tweets)
 
             if limit and num_tweets >= limit:
-                logger.info("Got {} tweets for {}.".format(
+                logger.info('Got {} tweets for {}.'.format(
                     num_tweets, query))
                 return
 
     except KeyboardInterrupt:
-        logger.info("Program interrupted by user. Returning tweets gathered "
-                     "so far...")
+        logger.info('Program interrupted by user. Returning tweets gathered '
+                     'so far...')
     except BaseException:
-        logger.exception("An unknown error occurred! Returning tweets "
-                          "gathered so far.")
-    logger.info("Got {} tweets for {}.".format(
+        logger.exception('An unknown error occurred! Returning tweets '
+                          'gathered so far.')
+    logger.info('Got {} tweets for {}.'.format(
         num_tweets, query))
 
 
@@ -186,15 +186,15 @@ def query_tweets(query, limit=None, begindate=dt.date(2006,3,21), enddate=dt.dat
     all_tweets = []
     try:
         pool = Pool(poolsize)
-        print('queries', queries)
+        logger.info('queries: {}'.format(queries))
         try:
             for new_tweets in pool.imap_unordered(partial(query_tweets_once, limit=limit_per_pool, lang=lang), queries):
                 all_tweets.extend(new_tweets)
-                logger.info("Got {} tweets ({} new).".format(
+                logger.info('Got {} tweets ({} new).'.format(
                     len(all_tweets), len(new_tweets)))
         except KeyboardInterrupt:
-            logger.info("Program interrupted by user. Returning all tweets "
-                         "gathered so far.")
+            logger.info('Program interrupted by user. Returning all tweets '
+                         'gathered so far.')
     finally:
         pool.close()
         pool.join()

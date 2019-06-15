0b37442ebd1ea42611dc1d13c088a8889b36ddb5
Ahmet Taspinar <taspinar@gmail.com>
2019-06-15 19:30:12 +0200

    Userprofile fixes (#173)
    * move scripts with example code to get twitter user data to examples folder
    
    * user.py:
    
    * user.py: change default values of __init__
    
    * query.py: remove redundant return
    
    * update setup.py, changelog, LICENSE, version number
    
    * README: add section for userprofile scraping info
    
    * main.py: add command line argument for user profile scraping

--- twitterscraper/__init__.py
+++ twitterscraper/__init__.py
@@ -1,15 +1,18 @@
 # TwitterScraper
-# Copyright 2016-2018 Ahmet Taspinar
+# Copyright 2016-2019 Ahmet Taspinar
 # See LICENSE for details.
 """
 Twitter Scraper tool
 """
 
-__version__ = '0.9.3'
+__version__ = '1.0.0'
 __author__ = 'Ahmet Taspinar'
 __license__ = 'MIT'
 
 
 from twitterscraper.query import query_tweets
+from twitterscraper.query import query_tweets_from_user
+from twitterscraper.query import query_user_info
 from twitterscraper.tweet import Tweet
+from twitterscraper.user import User
 from twitterscraper.ts_logger import logger as ts_logger

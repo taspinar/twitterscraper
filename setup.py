e99bac4f009f4fdde4953235c2b80a7822a87f1a
Ahmet Taspinar <taspinar@gmail.com>
2020-07-22 17:52:43 +0300

    update changelog, setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as requirements:
 
 setup(
     name='twitterscraper',
-    version='1.5.0',
+    version='1.6.0',
     description='Tool for scraping Tweets',
     url='https://github.com/taspinar/twitterscraper',
     author=['Ahmet Taspinar', 'Lasse Schuirmann'],
